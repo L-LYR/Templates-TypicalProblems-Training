01d43053262b0dc582308666d4564575cb08c549
L-LYR <1439581014@qq.com>
2020-11-10 19:19:16 +0800

    TEST

--- AtCoder/DP/N.cpp
+++ AtCoder/DP/N.cpp
@@ -1,27 +1,38 @@
-#include <iostream>
+#include <algorithm>
 #include <cstdio>
 #include <cstring>
-#include <algorithm>
-
+#include <iostream>
 using namespace std;
 using ll = long long;
-const int inf = 0x3f3f3f3f;
+
+const ll inf = 0x3f3f3f3f3f3f3f3f;
 const int maxn = 405;
 
-int a[maxn];
+ll a[maxn], sum[maxn];
 ll dp[maxn][maxn];
 
-int main(void){
+int main(void) {
     int n;
     cin >> n;
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    memset(dp, 0x3f, sizeof(dp));
+    for (int i = 1; i <= n; ++i) cin >> a[i];
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++ i) {
-            
+    // prefix sum
+    for (int i = 1; i <= n; ++i)
+        sum[i] = sum[i - 1] + a[i];
+
+    memset(dp, 0, sizeof(dp));
+    for (int l = 2; l <= n; ++l) {
+        for (int i = 1; i <= n - l + 1; ++i) {
+            int j = i + l - 1;
+            ll cur = inf;
+            for (int k = i; k < j; ++k) {
+                cur = min(cur,
+                          dp[i][k] + dp[k + 1][j] + sum[j] - sum[i - 1]);
+            }
+            dp[i][j] = cur;
         }
     }
+    cout << dp[1][n] << endl;
 
     return 0;
 }
